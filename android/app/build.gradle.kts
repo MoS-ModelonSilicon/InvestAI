b9234ad136b66c8d9cfd35d102a435cca87de806
YaronKlein <yaron.klein@intel.com>
2026-03-04 14:32:40 +0200

    chore: bump versionCode to 8 (v1.3.0), document +2 version rule

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "com.investai.app"
         minSdk = 26
         targetSdk = 36
-        versionCode = 6
-        versionName = "1.2.0"
+        versionCode = 8
+        versionName = "1.3.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
