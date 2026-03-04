ed9c75ea9798fda1218eb039e5f46ae3577e21bd
YaronKlein <yaron.klein@intel.com>
2026-03-04 13:45:02 +0200

    chore: bump versionCode to 6 (1.2.0) for Play Store

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "com.investai.app"
         minSdk = 26
         targetSdk = 36
-        versionCode = 4
-        versionName = "1.1.0"
+        versionCode = 6
+        versionName = "1.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
